b4631a05370601999d97640aa55e177899301fcc
SHIVANSH <75574324+Error-Shivansh@users.noreply.github.com>
2021-06-25 09:39:38 +0530

    Update __main__.py

--- VenomMusic/__main__.py
+++ VenomMusic/__main__.py
@@ -2,9 +2,9 @@ import requests
 
 from pyrogram import Client as Bot
 
-from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from VenomMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 
-from DaisyXMusic.services.callsmusic import run
+from VenomMusic.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 
